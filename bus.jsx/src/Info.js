ed88fed37c34b6239db572422065ba381d8b702a
Nicole Zhu <n.zhu5646@berkeley.edu>
2019-04-21 13:26:15 -0700

    Info panel for selected buses

---
+++ bus.jsx/src/Info.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css'
+
+class Info extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tab: 'Info'
+    };
+  }
+
+  render() {
+    return(
+      <div class="ui top attached tabular menu">
+        <a class="active item" data-tab="Info">Info</a>
+        <a class="item" data-tab="Stops">Stops</a>
+      </div>
+      <div class="ui bottom attached active tab segment" data-tab="first">
+        First
+      </div>
+      <div class="ui bottom attached tab segment" data-tab="second">
+        Second
+      </div>
+      <div class="ui bottom attached tab segment" data-tab="third">
+        Third
+      </div>
+    );
+  }
+}
